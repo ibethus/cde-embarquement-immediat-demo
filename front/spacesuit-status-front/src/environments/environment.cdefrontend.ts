9f04513005f1e5d644817abf94116c0ddb408f12
Ivan Béthus <62145038+ibethus@users.noreply.github.com>
2025-03-03 23:46:59 +0000

    Déploiement front ok

---
+++ front/spacesuit-status-front/src/environments/environment.cdefrontend.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  apiURL: 'https://opulent-acorn-4v4qwx9rjpqc5xvq-3000.app.github.dev',
+  keycloackUrl: 'https://opulent-acorn-4v4qwx9rjpqc5xvq-8080.app.github.dev',
+  clientId: 'spacesuits-app-frontend',
+  realm: 'spacesuits',
+  production: 'false'
+};
